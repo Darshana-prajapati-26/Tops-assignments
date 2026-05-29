1604d1d03f23816cbb646738e1d4a06f0a54fca0
Darshana prajapati <darshanaprajapati6002@gmail.com>
2026-05-29 15:23:44 +0530

    Update 17_generetor_func.py

--- Module-6) Python Fundamentals/Module-6 Practical/17_generetor_func.py
+++ Module-6) Python Fundamentals/Module-6 Practical/17_generetor_func.py
@@ -1,13 +1,18 @@
 #Write a generator function that generates the first 10 even numbers.
 # Generator function to generate first 10 even numbers
-def even_numbers():
-    num = 2  # start from 2
-    count = 0
-    while count < 10:
-        yield num
-        num += 2
-        count += 1
 
-# Using the generator
+def even_numbers():
+    
+    count = 0
+
+    for i in range (1,25):
+        if i % 2 == 0:
+            yield i
+            count += 1
+
+
+            if count == 10:
+                break
+
 for even in even_numbers():
-    print(even)
+    print (even)
