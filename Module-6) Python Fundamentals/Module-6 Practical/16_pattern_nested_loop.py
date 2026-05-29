e398ec004cd257060ed4b41326434d9361f5d66d
Darshana prajapati <darshanaprajapati6002@gmail.com>
2026-05-29 14:58:19 +0530

    Update 16_pattern_nested_loop.py

--- Module-6) Python Fundamentals/Module-6 Practical/16_pattern_nested_loop.py
+++ Module-6) Python Fundamentals/Module-6 Practical/16_pattern_nested_loop.py
@@ -1,8 +1,22 @@
-#Practical Example 4: Print Pattern using Nested for loop
-# Program to print a right-angle triangle pattern
 
-rows = 5
-for i in range(1, rows + 1):
+# Print this pattern using nested for loop:
+
+# *
+# **
+# ***
+# ****
+# *****
+
+
+for i in range(1,6):
+
     for j in range(i):
-        print("*", end="")
+        print("*", end="" )
+    
     print()
+
+
+# without using end =""
+
+for i in range(1,6):
+    print("*" * i)
