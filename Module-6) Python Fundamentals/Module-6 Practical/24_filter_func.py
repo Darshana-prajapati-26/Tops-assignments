0ac4247173d245b2f4a52abb0302af765d3dcbbd
Darshana prajapati <darshanaprajapati6002@gmail.com>
2026-05-30 14:20:35 +0530

    Update 24_filter_func.py

--- Module-6) Python Fundamentals/Module-6 Practical/24_filter_func.py
+++ Module-6) Python Fundamentals/Module-6 Practical/24_filter_func.py
@@ -2,7 +2,7 @@
 # Program to filter out even numbers
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
-odd_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
 
 print("Original list:", numbers)
 print("Odd numbers:", odd_numbers)
